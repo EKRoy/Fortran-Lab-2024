1fcff351b41a4bc3e6ff6a8057d5540ecf50e64c
Eshwor Kumar Roy <ekroy16@gmail.com>
2024-12-05 17:54:16 +0600

    Add files via upload
    added codes

--- program_06.f90
+++ program_06.f90
@@ -8,17 +8,17 @@ read*,si
 if(si==0) exit
 print*,"Enter the value of a,b,c,d,r,x"
 read*,a,b,c,d,r,x
-if((r/=d) .and. (b/=0) ) then
+if((r==d).or.(b==0) ) then
+print*,"The value of u and v is undefined"
+else
 u=abs(a+b**(-1))/sin(r-d)
 print*,"the value of u is:",u
-else
-print*,"The value of u and v is undefined"
-end if
 if(u/=0) then
-v=(((c*u**(-1))-(u*cos(x)))/b
-print*,"The value of v is:",v
+v=(c*u**(-1)-u*cos(x))/b
+print*,"the value of v=",v
 else 
-print*,"The value of v is undefined"
+print*,"the value of u is undefined"
+end if
 end if
 end do
-end program empression
+end program expression
