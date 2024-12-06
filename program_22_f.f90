f444a5c4c1fc53c17dd162b9e2bdce625dc17172
Eshwor Kumar Roy <ekroy16@gmail.com>
2024-12-06 20:09:46 +0600

    Add files via upload
    added

--- program_22_f.f90
+++ program_22_f.f90
@@ -1,30 +1,25 @@
 program sum_of_series_02
 implicit none
 integer::si,i,n
-real::sum,x
-real,parameter ::pi=3.1414
-integer,allocatable,dimension(:)::a
+real::sum,x,fact
+real,parameter ::pi=3.1416
 do
 print*,"Enter the serial no:\=0"
 read*,si
 if(si==0) exit
 print*,"Enter the value of x,n"
 read*,x,n
-allocate(a(n))
 if(n==0) then
 print*,"Sum=0"
 else
 x=(pi*x)/180
+fact=1.0
 sum=x
-a(0)=1
 do i=1,n-1
-a(i)=a(i-1)*(2*i)*(2*i+1)
-end do
-do i=1,n-1
-sum=sum+((-1)**i)*(x**(2*i+1))/a(i)
+fact=fact*(2*i)*(2*i+1)
+sum=sum+((-1)**i)*(x**(2*i+1))/fact
 end do
 print*,"Sum of series:",sum	
-end if
-deallocate(a) 
+end if 
 end do
 end program sum_of_series_02
