6e97729f4591fd67a3598a255d6b02b7d98c68e0
Eshwor Kumar Roy <ekroy16@gmail.com>
2024-12-06 07:03:18 +0600

    Add files via upload
    added problem 12

--- program_12.f90
+++ program_12.f90
@@ -8,14 +8,14 @@ read*,si
 if(si==0) exit
 print*,"Enter the value of n"
 read*,n
-allocate(fib(0:n))               
-fib(0)=0
-fib(1)=1
-do i=2,n
+allocate(fib(n))               
+fib(1)=0
+fib(2)=1
+do i=3,n
 fib(i)=fib(i-1)+fib(i-2)
 end do
 print*,"The Fibonacci Sequence:"
-do i=0,n-1
+do i=1,n
 print*,fib(i)
 end do
 deallocate(fib)
