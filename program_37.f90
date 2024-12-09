07bd2492a7e7346f4f7d64899ef3968b5714e7ff
Eshwor Kumar Roy <ekroy16@gmail.com>
2024-12-09 22:58:53 +0600

    Add files via upload
    q

--- program_37.f90
+++ program_37.f90
@@ -1,8 +1,8 @@
 program matrix_det
 implicit none
 integer::i,j,si,n
-real:: det
-integer,dimension(:,:),allocatable::A
+real(8):: det
+real(8),dimension(:,:),allocatable::A
 do
 print*,"Enter the serial no:/=0"
 read*,si
@@ -22,10 +22,7 @@ det=A(1,1)*(A(2,2)*A(3,3)-A(2,3)*A(3,2))-A(1,2)*(A(2,1)*A(3,3)-A(2,3)*A(3,1))+A(
 print*,"Determinant=",det
 else 
 print*,"Please Enter the size n=1 or 2 or 3"
-end if
-do i=1,n
-print*,(A(i,j),j=1,n)
-end do 
+end if 
 deallocate(A)
 end do
 end program
