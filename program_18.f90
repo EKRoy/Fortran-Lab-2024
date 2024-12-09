da6303636c17bc70976962c97162bd22be628ace
Eshwor Kumar Roy <ekroy16@gmail.com>
2024-12-09 18:30:18 +0600

    Add files via upload
    add

--- program_18.f90
+++ program_18.f90
@@ -2,20 +2,27 @@ program sum_of_expression_02
 implicit none
 integer::si,i,n
 real::sum,a,b
+logical::flag
 do
 print*,"Enter the serial no:/=0"
 read*,si
 if(si==0) exit
 print*,"Enter the value of n,a,b"
 read*,n,a,b
-if(a+(n-1)*b) then
-print*,"It is undefined"
-else
+flag=.true.
 sum=0.0
 do i=1,n
+if((a+(i-1)*b)==0) then
+flag=.false.
+exit
+else
 sum=sum+i/(a+(i-1)*b)
+end if
 end do
+if(flag) then
 print*,"Summation=",sum
+else 
+print*,"It is undefined"
 end if
 end do
 end program sum_of_expression_02
