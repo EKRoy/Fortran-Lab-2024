e2339064e8b6f080f38fc149758ac0217e4e5861
Eshwor Kumar Roy <ekroy16@gmail.com>
2024-12-09 09:18:09 +0600

    Add files via upload
    added codes

--- program_16.f90
+++ program_16.f90
@@ -3,15 +3,19 @@ implicit none
 integer::si,i,n
 real::prod,a,b,r
 do
-prInt*,"Enter the serial no:/=0"
+print*,"Enter the serial no:/=0"
 read*,si
 if(si==0) exit
 print*,"Enter the value of n,a,b,r"
 read*,n,a,b,r
+if(r+(n-1)*a==0) then
+print*,"It is Undefined"
+else
 prod=1.0
 do i=1,n
 prod=prod*(i*b)/(r+(i-1)*a)**i
 end do
 print*,"Product=",prod
+end if
 end do
 end program product_of_expression_02
