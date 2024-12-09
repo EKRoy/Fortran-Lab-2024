743d0f6edc86929eddbd4aa8bdfb78f93fd6bc2f
Eshwor Kumar Roy <ekroy16@gmail.com>
2024-12-09 09:20:18 +0600

    Add files via upload
    added codds

--- program_18.f90
+++ program_18.f90
@@ -8,10 +8,14 @@ read*,si
 if(si==0) exit
 print*,"Enter the value of n,a,b"
 read*,n,a,b
+if(a+(n-1)*b) then
+print*,"It is undefined"
+else
 sum=0.0
 do i=1,n
 sum=sum+i/(a+(i-1)*b)
 end do
 print*,"Summation=",sum
+end if
 end do
 end program sum_of_expression_02
