4c92150d8a8424dec8e88e72057ad816453f0771
Eshwor Kumar Roy <ekroy16@gmail.com>
2024-12-11 10:48:59 +0600

    Add files via upload
    add

--- program_39_ff.f90
+++ program_39_ff.f90
@@ -2,77 +2,70 @@ program experiment_39
 implicit none
 integer::n,i
 real::am,gm,sm,sd,am1,gm1,sm1,sd1
-integer,allocatable,dimension(:)::array
+integer,allocatable,dimension(:)::A
 print*,"Enter the value of array size:"
 read*,n
-allocate(array(n))
+allocate(A(n))
 print*,"Enter the elements:"
-read*,(array(i),i=1,n)
-sm1=sm(array,n)
-gm1=gm(array,n)
-am1=am(array,n)
-sd1=sd(array,n)
+read*,(A(i),i=1,n)
+sm1=sm(A,n)
+gm1=gm(A,n)
+am1=am(A,n)
+sd1=sd(A,n,am1)
 print*,"sum=",sm1
 print*,"GM=",gm1
 print*,"AM=",am1
 print*,"SD=",sd1
-deallocate(array)
+deallocate(A)
 end program experiment_39 
 
-real function sm(arr,n)
+real function sm(A,n)
 implicit none
 integer::i
-integer,intent(in)::n,arr(n)
+integer,intent(in)::n,A(n)
 real::sum
 sum=0.0
 do i=1,n
-sum=sum+arr(i)
+sum=sum+A(i)
 end do
 sm=sum
 end function sm
 
-real function gm(arr,n)
+real function gm(A,n)
 implicit none
 integer::i
-integer,intent(in)::n,arr(n)
-real::product,gm1
+integer,intent(in)::n,A(n)
+real::product
 product=1.0
 do i=1,n
-product=product*arr(i)
+product=product*A(i)
 end do
-gm1=product**(1/real(n))
-gm=gm1
+gm=product**(1/real(n))
 end function gm
 
-real function am(arr,n)
+real function am(A,n)
 implicit none
 integer::i
-integer,intent(in)::n,arr(n)
-real::sum,arithm
+integer,intent(in)::n,A(n)
+real::sum
 sum=0.0
 do i=1,n 
-sum=sum+arr(i)
+sum=sum+A(i)
 end do
-arithm=sum/real(n)
-am=arithm
+am=sum/real(n)
 end function am
 
-real function sd(arr,n)
+real function sd(A,n,am1)
 implicit none
 integer::i
-integer,intent(in)::n,arr(n)
-real::dv,sum,amm,val
-sum=0.0
-do i=1,n
-sum=sum+arr(i)
-end do
-amm=sum/real(n)
+integer,intent(in)::n,A(n)
+real::dv,am1
 dv=0.0
 do i=1,n
-dv=dv+(arr(i)-amm)**2.0
+dv=dv+(A(i)-am1)**2.0
 end do
-val=dv/real(n)
-sd=sqrt(val)
+dv=dv/real(n)
+sd=sqrt(dv)
 end function sd
 
 
